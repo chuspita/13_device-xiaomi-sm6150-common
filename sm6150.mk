e8173466d26b4b4afff23545f06a3da186d0979e
chuspita <kosecki99@gmail.com>
2022-10-18 17:11:05 +0200

    sm6150-common: Hello Xtended!

--- sm6150.mk
+++ sm6150.mk
@@ -296,7 +296,7 @@ PRODUCT_PACKAGES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-spark
+    $(LOCAL_PATH)/overlay-xtended
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
